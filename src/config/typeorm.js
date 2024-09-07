e7bb14fe25dfc0fb420393319fe97755c3aa7999
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2024-09-07 19:45:23 +0500

    typeorm.js file fixed

--- src/config/typeorm.js
+++ src/config/typeorm.js
@@ -1,15 +1,18 @@
+require(`reflect-metadata`)
 const { DataSource } = require("typeorm");
 const userSchema = require("../entity/usersModel");
 const productSchema = require("../entity/productModel");
-require(`dotenv`).config()
+require(`dotenv`).config();
 
 const AppDataSource = new DataSource({
-    type: "postgres",
-    url:process.env.URL, // Connection String
-    entities: [userSchema, productSchema],
-    synchronize: false,
-    logging: false,
-  });
+  type: "postgres",
+  url: process.env.URL, // Connection String
+  entities: [userSchema, productSchema],
+  synchronize: false,
+  logging: false,
+  ssl: {
+    rejectUnauthorized: false, // SSL sertifikatini tekshirmaslik uchun
+  },
+});
 
-
-  module.exports = AppDataSource
+module.exports = AppDataSource;

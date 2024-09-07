41cc70c159655872ced1675949a1cf83c393f3dd
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2024-09-07 19:31:39 +0500

    synchronize: true,

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,15 +4,15 @@ const userSchema = require("../entity/usersModel");
 
 // Superadmin get all users
 const getAllUsers = async (req, res) => {
-  const userRepository = AppDataSource.getRepository("users")
-  const allUsers = await userRepository.find();
-
-  return res.status(200).send({
-    success: true,
-    message: "All users data",
-    data: allUsers,
-  });
   try {
+    const userRepository = AppDataSource.getRepository("users")
+    const allUsers = await userRepository.find();
+
+    return res.status(200).send({
+      success: true,
+      message: "All users data",
+      data: allUsers,
+    });
   } catch (error) {
     return res.status(error.status || 500).send({
       success: false,

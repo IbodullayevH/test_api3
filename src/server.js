2a6671bbc5590151f96862ce0855d156b0857e2f
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2024-09-11 16:10:37 +0500

    files fixed

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ require(`dotenv`).config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
+  
 // The database is connected via typeorm
 AppDataSource.initialize()
   .then(() => {
